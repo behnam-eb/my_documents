6653aa8e7241fc240b05e840f4e8bc4c16d55c81
Behnam Ebrahimpour <ebrahimpour.behnam@gmail.com>
2020-11-10 04:07:02 +0330

    11/10/2020

--- migration-chtoch_V2/ldaprestore.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-IP=`ifconfig |grep "inet addr"|grep -v "127.0.0.1"|awk '{print $2}'|sed 's/addr://'`
-
-/opt/Chmail/openldap/bin/ldapadd -c -x -w $(su - Chmail -c 'zmlocalconfig -s ldap_root_password '  | awk '{print $3}') -D "$(su - Chmail -c 'zmlocalconfig -s Chmail_ldap_userdn'  | awk '{print $3}')" -H ldap://"$IP" -f user.ldif
-
-
